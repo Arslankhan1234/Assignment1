5db2fa260f90057a057aca26576a62489bbc27a6
Arslan Khan <bsef19a018@pucit.edu.pk>
2022-05-23 11:56:12 -0700

    Create function new-activity which will call on clicking button

--- Learningapp/app/src/main/java/com/example/learningapp/MainActivity.java
+++ Learningapp/app/src/main/java/com/example/learningapp/MainActivity.java
@@ -5,25 +5,159 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-Button btn1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1=(Button)findViewById(R.id.A);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Button b=(Button) view;
-                if(b.getId()==R.id.A)
-                {
 
-                }
 
-            }
-        });
+    }
+
+    public void new_activity(View view) {
+        if(view.getId()==R.id.A)
+        {
+            Toast.makeText(getApplicationContext(),"A", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.B)
+        {
+            Toast.makeText(getApplicationContext(),"B", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else if(view.getId()==R.id.C)
+        {
+            Toast.makeText(getApplicationContext(),"C", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.D)
+        {
+            Toast.makeText(getApplicationContext(),"D", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.E)
+        {
+            Toast.makeText(getApplicationContext(),"E", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.F)
+        {
+            Toast.makeText(getApplicationContext(),"F", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.G)
+        {
+            Toast.makeText(getApplicationContext(),"G", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.H)
+        {
+            Toast.makeText(getApplicationContext(),"H", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.I)
+        {
+            Toast.makeText(getApplicationContext(),"I", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.J)
+        {
+            Toast.makeText(getApplicationContext(),"J", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else if(view.getId()==R.id.K)
+        {
+            Toast.makeText(getApplicationContext(),"K", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else if(view.getId()==R.id.L)
+        {
+            Toast.makeText(getApplicationContext(),"L", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else if(view.getId()==R.id.M)
+        {
+            Toast.makeText(getApplicationContext(),"M", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else if(view.getId()==R.id.N)
+        {
+            Toast.makeText(getApplicationContext(),"N", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+        else if(view.getId()==R.id.O)
+        {
+            Toast.makeText(getApplicationContext(),"O", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else if(view.getId()==R.id.P)
+        {
+            Toast.makeText(getApplicationContext(),"P", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.Q)
+        {
+            Toast.makeText(getApplicationContext(),"Q", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else if(view.getId()==R.id.R)
+        {
+            Toast.makeText(getApplicationContext(),"R", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.S)
+        {
+            Toast.makeText(getApplicationContext(),"S", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else if(view.getId()==R.id.T)
+        {
+            Toast.makeText(getApplicationContext(),"T", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.U)
+        {
+            Toast.makeText(getApplicationContext(),"U", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.V)
+        {
+            Toast.makeText(getApplicationContext(),"V", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.W)
+        {
+            Toast.makeText(getApplicationContext(),"W", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.X)
+        {
+            Toast.makeText(getApplicationContext(),"X", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.Y)
+        {
+            Toast.makeText(getApplicationContext(),"Y", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getId()==R.id.Z)
+        {
+            Toast.makeText(getApplicationContext(),"z", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
